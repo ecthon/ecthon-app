35413b9e03a021b0e667ff40e67bcb13070fc038
ecthon <ecthon@gmail.com>
2024-01-27 13:22:22 -0400

    feature: temporary menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,11 @@
-'use client'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Menu } from "./menu";
 
 export function Header() {
   return (
-    <header className="flex w-full items-center border-b fixed z-10">
-      <div className="flex w-[1216px] justify-between mx-auto py-5 max-sm:py-2 max-sm:px-5">
-        <h1 className="font-semibold text-xl max-sm:text-sm">ecthon<span className="text-purple-600">.</span></h1>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <HamburgerMenuIcon />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Menu</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer">Home</DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">Projetos</DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">Sobre</DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">Contato</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <header className="flex w-full items-center border-b bg-white/50 filter backdrop-blur-sm fixed">
+      <div className="flex w-[1216px] justify-between items-center mx-auto py-5 max-sm:py-2 max-sm:px-5">
+        <h1 className="font-semibold text-xl max-sm:text-sm">ecthon<span className="text-blue-600">.</span></h1>
+        <Menu />
       </div>
     </header>
   )
