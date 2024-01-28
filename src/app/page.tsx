3e69866f92c8782f5c1b9f442d36cb4436db1a46
ecthon <ecthon@gmail.com>
2024-01-28 06:11:07 -0400

    refactor: components/header: menu.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
+import { Header } from "@/components/header/header";
 import { Heading } from "@/components/heading";
 import { Projects } from "@/components/projects";
 
---
+++ src/components/header/menu.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Link from "next/link";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "../ui/navigation-menu";
+export function Menu() {
+
+  return(
+    <NavigationMenu className="max-sm:hidden">
+    <NavigationMenuList className="flex gap-1">
+      <Link href="#">
+        <NavigationMenuItem className="font-normal text-sm px-4 py-2 hover:text-black hover:bg-black/5 rounded-lg">
+          Home
+        </NavigationMenuItem>
+      </Link>
+      <Link href="#">
+        <NavigationMenuItem className="font-normal text-sm px-4 py-2 hover:text-black hover:bg-black/5 rounded-lg">
+          Projetos
+        </NavigationMenuItem>
+      </Link>
+      <Link href="#">
+        <NavigationMenuItem className="font-normal text-sm px-4 py-2 hover:text-black hover:bg-black/5 rounded-lg">
+          Sobre
+        </NavigationMenuItem>
+      </Link>
+      <Link href="#">
+        <NavigationMenuItem className="font-normal text-sm px-4 py-2 hover:text-black hover:bg-black/5 rounded-lg">
+          Contato
+        </NavigationMenuItem>
+      </Link>
+    </NavigationMenuList>
+  </NavigationMenu>
+  )
+}
