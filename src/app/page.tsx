ae7468352b0061fce7a7ab7f2fcbdebcb5494cc0
ecthon <ecthon@gmail.com>
2024-01-31 11:21:23 -0400

    feature: config theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { ModeToggle } from "@/components/theme/mode-toggle";
+
 export default function Home() {
   return (
-    <div className="flex flex-col bg-slate-50 w-full h-auto">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <ModeToggle />
+    </>
   )
 }
