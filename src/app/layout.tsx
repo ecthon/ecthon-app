383085ab8e62f06574b3ca75e2673601fab813ef
ecthon <ecthon@gmail.com>
2024-01-31 10:29:43 -0400

    npm update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from '@/components/theme/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,13 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          enableSystem
-          disableTransitionOnChange
-        >
           {children}
-        </ThemeProvider>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header/header";
-import { Heading } from "@/components/heading";
-import { Projects } from "@/components/projects";
-
 export default function Home() {
   return (
     <div className="flex flex-col bg-slate-50 w-full h-auto">
-      <Header />
-      <Heading />
-      <Projects />
-      <Footer />
+      <h1>Hello World</h1>
     </div>
   )
 }
