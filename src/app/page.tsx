8ed528242c76d31878770b284554c0e094f47b3d
ecthon <ecthon@gmail.com>
2024-02-24 10:18:53 -0400

    feature: init home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <>
-    <div>Hello</div>
-    </>
+    <div className="flex flex-col w-[928px] bg-zinc-300 mx-auto">
+      <span>Disponível para freelace</span>
+    </div>
   )
 }
