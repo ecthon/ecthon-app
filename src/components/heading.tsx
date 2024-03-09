9620474cf7980ff7841b493ad07760393c5bd7fb
ecthon <ecthon@gmail.com>
2024-03-09 19:51:58 -0400

    fix: components margins

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -5,7 +5,7 @@ import { FaLinkedinIn } from "react-icons/fa"
 
 export function Heading() {
   return (
-    <div className="flex flex-col items-center justify-center mx-auto w-[928px] max-sm:w-full h-screen max-sm:px-5">
+    <div className="flex flex-col items-center justify-center mx-auto w-[928px] max-sm:w-full h-screen max-sm:-mt-28 max-sm:px-5">
       
     <div className="flex items-center rounded-lg bg-emerald-500/5 gap-2 px-3 py-1 text-sm font-medium mx-auto">
       <div className="h-2 w-2 rounded-full bg-emerald-500">
@@ -14,7 +14,7 @@ export function Heading() {
         <Separator orientation="vertical" className="h-4 dark:bg-white/10 bg-black/10"/>
       <span className="text-zinc-900 antialiased max-sm:text-[11px]">DISPONÍVEL PARA <span className="font-bold">FREELANCE</span></span>
     </div>
-    <h1 className=" flex max-sm:flex-col max-sm:w-full mx-auto text-center text-5xl max-sm:text-4xl font-bold mt-8 text-zinc-900 antialiased">Front-end & UI Designer ✨.</h1>
+    <h1 className=" flex max-sm:flex-col max-sm:w-full mx-auto text-center text-5xl max-sm:text-4xl font-bold mt-8 text-zinc-900 antialiased">Front-end & <br className="hidden max-sm:flex" /> UI Designer ✨.</h1>
     <p className="text-zinc-700 max-sm:w-full max-w-[750px] text-center text-lg text-muted-foreground max-sm:text-sm justify-center mt-6 mb-8">
       Desenvolvendo soluções criativas e inovadoras, sempre com o objetivo de proporcionar a melhor experiência ao usuário.
     </p>
