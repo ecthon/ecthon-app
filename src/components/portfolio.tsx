5d6530cbb65638fa998bfbd53e0a9bb1399b1927
ecthon <ecthon@gmail.com>
2024-03-09 15:16:38 -0400

    style: responsive  portfolio section

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -3,16 +3,16 @@ import { Card } from "./ui/card";
 
 export function Portfolio() {
   return (
-    <div className="flex flex-col w-[1216px] mx-auto gap-8 -mt-60">
+    <div className="flex flex-col w-[1216px] max-sm:w-full mx-auto gap-8 -mt-60 max-sm:px-5">
       <div className="flex gap-1 items-center">
         <h1 className="font-extralight text-3xl">Portfolio</h1>
         <span><HiMiniArrowDownRight size={24}/></span>
       </div>
       
-      <div className="grid grid-cols-6 grid-rows-6 gap-8">
-          <Card className="h-[500px] col-span-3 row-span-6 bg-rose-500 rounded-2xl"></Card>
-          <Card className="col-span-3 row-span-3 col-start-4 bg-purple-600 rounded-2xl"></Card>
-          <Card className="col-span-3 row-span-3 col-start-4 row-start-4 bg-emerald-500 rounded-2xl"></Card>
+      <div className="grid grid-cols-6 grid-rows-6 gap-8 max-sm:gap-4 max-sm:flex max-sm:flex-col max-sm:w-full">
+          <Card className="max-sm:flex max-sm:w-full h-[500px] max-sm:h-[400px] col-span-3 row-span-6 bg-rose-500 rounded-2xl"></Card>
+          <Card className="max-sm:flex max-sm:w-full max-sm:h-[400px] col-span-3 row-span-3 col-start-4 bg-purple-600 rounded-2xl"></Card>
+          <Card className="max-sm:flex max-sm:w-full max-sm:h-[400px] col-span-3 row-span-3 col-start-4 row-start-4 bg-emerald-500 rounded-2xl"></Card>
       </div>
     </div>
   )
