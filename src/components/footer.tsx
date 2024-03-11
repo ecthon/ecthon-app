39a351cb664f1209b31bc259a3aa04ce41d64909
ecthon <ecthon@gmail.com>
2024-03-11 00:02:49 -0400

    feature: add timezone in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { GetTimeZone } from "./get-time-zone"
 import { Separator } from "./ui/separator"
 
 export function Footer() {
@@ -5,7 +6,10 @@ export function Footer() {
   return (
     <footer className="flex flex-col w-[1216px] max-sm:w-full max-sm:px-5 mx-auto mt-20">
       <Separator orientation="horizontal" className="w-full dark:bg-zinc-800"/>
-      <p className="font-normal text-zinc-400 dark:text-zinc-500 text-sm py-4 max-sm:text-[11px]">&copy; {currentYear} — Feito com ❤️ por  @ecthon.<br className="hidden max-sm:flex" /> Todos os direitos reservados.</p>
+      <div className="flex max-sm:w-full w-full items-center justify-between">
+        <p className="font-normal text-zinc-400 dark:text-zinc-500 text-sm py-4 max-sm:text-[11px]">&copy; {currentYear} — Feito com ❤️ por  @ecthon.<br className="hidden max-sm:flex" /> Todos os direitos reservados.</p>
+        <GetTimeZone />
+      </div>
     </footer>
   )
 }
---
+++ src/components/get-time-zone.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import { FaRegClock } from "react-icons/fa6";
+
+export function GetTimeZone() {
+  const [formattedDateTime, setFormattedDateTime] = useState('');
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const dateTime = new Date();
+      const options: Intl.DateTimeFormatOptions = {
+        hour12: true,
+        // timeZoneName: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        // second: '2-digit',
+        timeZone: 'America/Manaus'
+      };
+    
+      const formattedDateTimeString = new Intl.DateTimeFormat('pt-BR', options).format(dateTime);
+      setFormattedDateTime(formattedDateTimeString);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures this effect runs only once, similar to componentDidMount
+
+  return (
+      <span className='flex px-3 py-1 rounded-full bg-zinc-900 items-center gap-1 font-normal text-zinc-400 dark:text-zinc-500 text-sm max-sm:text-[11px]'>
+      <FaRegClock size={16} />
+        {formattedDateTime}
+      </span>
+  );
+}
