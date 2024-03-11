8774d88ba5ae32057858aed97b41b2960e7f03b9
ecthon <ecthon@gmail.com>
2024-03-10 22:42:11 -0400

    Footer info

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className="flex flex-col w-[1216px] max-sm:w-full max-sm:px-5 mx-auto mt-20">
       <Separator orientation="horizontal" className="w-full dark:bg-zinc-800"/>
-      <p className="font-normal text-zinc-400 dark:text-zinc-500 text-sm py-4 max-sm:text-[11px]">&copy; {currentYear} — Ecthon Almeida.<br className="hidden max-sm:flex" /> Todos os direitos reservados.</p>
+      <p className="font-normal text-zinc-400 dark:text-zinc-500 text-sm py-4 max-sm:text-[11px]">&copy; {currentYear} — Feito com ❤️ por  @ecthon.<br className="hidden max-sm:flex" /> Todos os direitos reservados.</p>
     </footer>
   )
 }
