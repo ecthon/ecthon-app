855fd837caab3e7f7352b8c34848d203ca00f12c
ecthon <ecthon@gmail.com>
2025-02-09 23:17:54 -0400

    feat: alterar fonte global para "Red Hat Display" e ajustar cores de texto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Space_Mono } from "next/font/google";
+import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 
-const spaceMono = Space_Mono({
+const redHatDisplay = Red_Hat_Display({
   weight: ["400", "700"],
   variable: "--font-space-mono",
   subsets: ["latin"],
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${spaceMono.variable} antialiased`}>
+      <body className={`${redHatDisplay.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
       </div>
       <header className="flex flex-col w-full">
         <h1 className="font-semibold text-xl md:text-2xl">Ecthon Almeida</h1>
-        <p className="text-zinc-400">Design Engineer</p>
+        <p className="text-zinc-500">Design Engineer</p>
       </header>
       <div>
-        <p className="text-zinc-400 text-sm md:text-base">
+        <p className="text-zinc-500 text-sm md:text-base">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis
           consequatur reiciendis enim fugiat nobis velit ut, amet expedita?
           Sequi excepturi nostrum eveniet quasi debitis. Est temporibus facere
@@ -32,7 +32,7 @@ export default function Home() {
         <div className="flex w-full justify-between">
           <h2 className="font-semibold text-lg md:text-xl">Projetos</h2>
           <div className="group flex items-center gap-2">
-            <button className="text-zinc-400 text-sm md:text-base">
+            <button className="text-zinc-600 text-sm md:text-base">
               ver todos
             </button>
             <ArrowRight02Icon className="size-0 group-hover:size-4 md:group-hover:size-6 transition-all duration-300" />
