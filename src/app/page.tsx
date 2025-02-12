ff2d6d16f428e9baddce6fe85700aa9a2b3bd99c
ecthon <ecthon@gmail.com>
2025-02-11 23:38:35 -0400

    feat: atualizar layout e estilo do componente Profile, incluindo novas informações e botão de mensagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 import CardHoverEffectDemo from "@/components/card-hover-effect-demo";
 import { Footer } from "@/components/footer";
-import { Profile } from "@/components/profile";
 import { ArrowRight02Icon } from "hugeicons-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full max-w-[600px] mx-auto text-base px-4 py-6 md:py-10 space-y-8">
-      {/* <Header /> */}
-      <Profile />
+    <div className="flex flex-col w-full h-screen max-w-[600px] mt-10 mx-auto text-base md:py-10 space-y-8">
       <div className="flex flex-col">
         <div className="flex w-full justify-between px-4">
           <h2 className="font-semibold text-sm">Projetos</h2>
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -3,13 +3,17 @@ import { Button } from "./ui/button";
 
 export function Profile() {
   return (
-    <div className="flex flex-col w-full h-[200px] justify-center rounded-2xl space-y-6 px-8 bg-cover bg-center bg-[url('/bg02.png')]">
+    <div className="flex flex-col w-full h-auto justify-center max-sm:rounded-none rounded-2xl space-y-6 px-8 py-8 bg-cover bg-center bg-[url('/bg02.png')]">
       <div className="flex flex-col text-sm space-y-2">
-        <h1 className="font-bold text-xl">ecthon almeida</h1>
-        <p className="text-zinc-500 text-md">Design Engineer</p>
+        <p className="text-zinc-500 text-xs">ecthon almeida</p>
+        <h1 className="font-bold text-xl">Desenvolvedor & UI Design</h1>
+        <p className="text-zinc-300">
+          Precisando de uma ajuda com desenvolvimento ou design do seu projeto?
+          Eu posso ajudar.
+        </p>
       </div>
       <div className="flex items-center">
-        <Button className="bg-zinc-900 text-zinc-50 hover:bg-zinc-800 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-200 font-semibold flex items-center gap-2">
+        <Button className="justify-center bg-zinc-900 rounded-full text-zinc-50 hover:bg-zinc-800 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-200 font-semibold flex items-center gap-2">
           <Message02Icon />
           Enviar mensagem
         </Button>
