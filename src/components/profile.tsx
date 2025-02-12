3584f3c0eaa57f32b78258fc02d40103205a29e4
ecthon <ecthon@gmail.com>
2025-02-11 22:25:39 -0400

    feat: remove unused SVG files and clean up Profile component styles

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,15 +1,10 @@
 export function Profile() {
   return (
-    <div className="flex flex-col w-full space-y-4 px-4 border-b-2">
+    <div className="flex flex-col w-full space-y-4 px-4 border-b-2 bg-cover bg-center bg-[url('/bg-home.svg')]">
       <div className="flex flex-col text-sm space-y-1">
-        <h1 className="font-bold">Ecthon Almeida</h1>
-        <p className="text-zinc-500 text-xs">Design Engineer</p>
+      <h1 className="font-bold">Ecthon Almeida</h1>
+      <p className="text-zinc-500 text-xs">Design Engineer</p>
       </div>
-      {/* <p className="text-zinc-500 text-sm">
-        Sou um desenvolvedor apaixonado por criar soluções elegantes e
-        eficientes. Trabalho com TypeScript, React e Tailwind, e gosto de design
-        minimalista.
-      </p> */}
     </div>
   );
 }
