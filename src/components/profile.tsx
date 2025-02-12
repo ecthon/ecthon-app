6dbc46c32e3bd0666a7544246e261cf45daf3b9a
ecthon <ecthon@gmail.com>
2025-02-11 23:05:37 -0400

    feat: atualizar componente Profile com novo layout, estilo e botão de mensagem

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,9 +1,18 @@
+import { Message02Icon } from "hugeicons-react";
+import { Button } from "./ui/button";
+
 export function Profile() {
   return (
-    <div className="flex flex-col w-full space-y-4 px-4 border-b-2 bg-cover bg-center bg-[url('/bg-home.svg')]">
-      <div className="flex flex-col text-sm space-y-1">
-      <h1 className="font-bold">Ecthon Almeida</h1>
-      <p className="text-zinc-500 text-xs">Design Engineer</p>
+    <div className="flex flex-col w-full h-[200px] justify-center rounded-2xl space-y-6 px-8 bg-cover bg-center bg-[url('/bg02.png')]">
+      <div className="flex flex-col text-sm space-y-2">
+        <h1 className="font-bold text-xl">ecthon almeida</h1>
+        <p className="text-zinc-500 text-md">Design Engineer</p>
+      </div>
+      <div className="flex items-center">
+        <Button className="bg-zinc-900 text-zinc-50 hover:bg-zinc-800 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-200 font-semibold flex items-center gap-2">
+          <Message02Icon />
+          Enviar mensagem
+        </Button>
       </div>
     </div>
   );
