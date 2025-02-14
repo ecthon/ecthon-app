fd1115922b0459f71e4675659bcd6cc3dbaf45a7
ecthon <ecthon@gmail.com>
2025-02-14 01:35:27 -0400

    feat: adicionar suporte a responsividade no componente Home, ajustando estilos de texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Header } from "@/components/header";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
         <Header />
         <main className="flex flex-col w-full h-[400px] gap-6 align-middle justify-center">
           <div className="flex items-center space-x-4 border rounded-full px-2 py-0.5 bg-zinc-900 w-[fit-content]">
-            <p className="font-semibold text-white flex items-center gap-2 space-x-1">
+            <p className="font-semibold text-white flex items-center gap-2 space-x-1 max-sm:space-x-0 text-xs sm:text-sm md:text-base">
               <span className="font-thin text-xs">👋</span>
               Disponível para projetos
               <span>•</span>
