8127ef0e173b3e39187da619af480024581ab309
ecthon <ecthon@gmail.com>
2025-02-15 02:24:21 -0400

    feat: update PortfolioSection styles for improved layout and visual consistency

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -2,9 +2,9 @@ export function PortfolioSection() {
   return (
     <div className="w-full md:px-0 mx-auto max-w-full md:max-w-full">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="bg-white p-4 shadow-md rounded-lg">Card 1</div>
-        <div className="bg-white p-4 shadow-md rounded-lg">Card 2</div>
-        <div className="bg-white p-4 shadow-md rounded-lg">Card 3</div>
+        <div className="flex h-[300px] bg-zinc-900 p-4 shadow-md rounded-lg">Card 1</div>
+        <div className="flex h-[300px] bg-zinc-900 p-4 shadow-md rounded-lg">Card 2</div>
+        <div className="flex h-[300px] bg-zinc-900 p-4 shadow-md rounded-lg">Card 3</div>
       </div>
     </div>
   );
