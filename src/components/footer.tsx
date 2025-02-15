ded41975d72a0a5441b68e0168e9f7ba15301b39
ecthon <ecthon@gmail.com>
2025-02-15 04:01:19 -0400

    feat: update layout and footer components, remove unused images, and enhance clock functionality

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,8 +7,10 @@ import { useEffect, useState } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
+    setMounted(true);
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
@@ -16,24 +18,25 @@ function Clock() {
     return () => clearInterval(timer);
   }, []);
 
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <div className="flex items-center gap-2 px-2 py-1 rounded-lg border border-zinc-900">
+    <div className="flex items-center justify-center gap-2 px-2 py-1 rounded-lg border border-zinc-900">
       <DigitalClockIcon size={16} />
-      <p className="font-bold">{time.toLocaleTimeString()}</p>
+      <p className="flex w-[55px] text-xs text-zinc-500 font-bold">{time.toLocaleTimeString()}</p>
     </div>
   );
 }
 
 export function Footer() {
   return (
-    <footer className="flex flex-col md:flex-row items-center gap-4 justify-between py-4">
+    <footer className="flex w-full md:flex-row items-center justify-between py-4 mt-10">
       <Link href={"/"} className="font-bold text-sm py-1 px-2 rounded-md">
       ecthon.<span className="text-zinc-700">almeida</span>
       </Link>
-
-      <div className="text-zinc-500 text-[11px] w-full md:w-[90px] px-2 flex items-center gap-2 justify-center md:justify-start">
       <Clock />
-      </div>
     </footer>
   );
 }
