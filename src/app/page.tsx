91b2ab26530039fced6e6476294c6e788c29bb6f
ecthon <ecthon@gmail.com>
2025-02-15 01:52:50 -0400

    feat: add PortfolioSection component to Home, enhancing project showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/hero-section";
+import { PortfolioSection } from "@/components/portfolio";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="flex flex-col w-full px-4 md:px-6 lg:w-[1120px] mx-auto">
         <Header />
         <HeroSection />
+        <PortfolioSection />
       </div>
     </div>
   );
