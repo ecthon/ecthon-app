261d0e7fb41475a4943e7059e509d1755c1a15b1
ecthon <ecthon@gmail.com>
2025-02-15 12:33:34 -0400

    feat: reorder Footer component elements for improved layout and readability

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -33,13 +33,13 @@ function Clock() {
 export function Footer() {
   return (
     <footer className="flex flex-col w-full py-4 md:py-6 space-y-4">
-      <Link href={"/"} className="font-bold text-sm py-1 rounded-md mb-2 md:mb-0">
-      ecthon.<span className="text-zinc-700">almeida</span>
-      </Link>
       <div className="flex flex-col md:flex-row justify-between items-center w-full space-y-2 md:space-y-0">
-      <p className="text-xs text-zinc-700 font-semibold">&copy; {new Date().getFullYear()} Ecthon. All rights reserved.</p>
-      <Clock />
+        <Link href={"/"} className="font-bold text-sm py-1 rounded-md mb-2 md:mb-0">
+        ecthon.<span className="text-zinc-700">almeida</span>
+        </Link>
+        <Clock />
       </div>
+      <p className="text-xs text-zinc-700 font-semibold">&copy; {new Date().getFullYear()} Ecthon. All rights reserved.</p>
     </footer>
   );
 }
