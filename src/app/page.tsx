1e765a2ce6fcb89ef2085bd33fa8305cf05a9066
ecthon <ecthon@gmail.com>
2025-07-01 13:12:18 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
+import { HugeiconsIcon } from "@hugeicons/react";
+import { GithubIcon, Linkedin02Icon, Location01Icon } from '@hugeicons/core-free-icons'
+
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col w-full space-y-6">
+      <header className="flex items-center justify-between py-4 px-10 text-white border-b border-zinc-200">
+        <Link href={'#'} className="text-zinc-900 font-bold">Base²²</Link>
+        <div>
+          <Image src={'https://github.com/ecthon.png'} alt="Avatar" width={24} height={24} className="rounded-full" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </header>
+
+      <div className="flex w-[1112px] mx-auto items-center justify-between p-4">
+        <div className="flex items-center space-x-4">
+          <Image src={'https://github.com/ecthon.png'} alt="Avatar" width={64} height={64} className="rounded-full" />
+          <div className="flex flex-col">
+            <p className="font-semibold text-zinc-900">Ecthon Almeida</p>
+            <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-1">
+                <HugeiconsIcon icon={Location01Icon} size={14} strokeWidth={1.5} />
+                <p className="text-sm text-zinc-500">Manaus</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center space-x-1">
+          <Link href={'#'} className="group flex p-1.5 border border-zinc-200 rounded-lg hover:bg-zinc-100 transition-colors">
+            <HugeiconsIcon icon={Linkedin02Icon} size={18} strokeWidth={2} className="text-zinc-500" />
+          </Link>
+          <Link href={'#'} className="group flex p-1.5 border border-zinc-200 rounded-lg hover:bg-zinc-100 transition-colors">
+            <HugeiconsIcon icon={GithubIcon} size={18} strokeWidth={2} className="text-zinc-500" />
+          </Link>
+          <button className="flex py-1.5 px-2 rounded-lg cursor-pointer font-semibold bg-zinc-100 text-sm">Send mensagem</button>
+        </div>
+      </div>
+
+      <div className="w-[1112px] mx-auto flex gap-4">
+        <div className="group cursor-pointer w-[360px] h-[420px] border border-zinc-100 rounded-2xl p-1">
+          <div className="flex flex-col justify-end h-full bg-gray-100 rounded-[12px] overflow-hidden">
+            <div className="flex w-full items-center p-4 bg-white/20 backdrop-blur-sm">
+              <h2 className="text-md font-semibold text-zinc-900">Nome do projeto.</h2>
+            </div>
+          </div>
+        </div>
+        <div className="w-[360px] bg-gray-100 p-4 rounded-2xl">
+          <h2 className="text-lg font-semibold mb-2">Card 2</h2>
+          <p>Conteúdo do card 2.</p>
+        </div>
+        <div className="w-[360px] bg-gray-100 p-4 rounded-2xl">
+          <h2 className="text-lg font-semibold mb-2">Card 3</h2>
+          <p>Conteúdo do card 3.</p>
+        </div>
+      </div>
     </div>
   );
 }
