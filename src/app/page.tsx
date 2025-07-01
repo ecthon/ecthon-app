2d647f831c97e150e2da8771898f5061f90af2d9
ecthon <ecthon@gmail.com>
2025-07-01 15:34:27 -0400

    Refactor layout and styling in Home component for improved responsiveness and visual consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col w-full space-y-6">
-      <header className="flex items-center justify-between py-4 px-10 text-white border-b border-zinc-200">
+      <header className="flex items-center justify-between py-4 px-4 md:px-10 text-white border-b border-zinc-200">
         <Link href={'#'} className="text-zinc-900 font-bold">Base²²</Link>
         <div>
           <Image src={'https://github.com/ecthon.png'} alt="Avatar" width={24} height={24} className="rounded-full" />
         </div>
       </header>
 
-      <div className="flex w-[1112px] mx-auto items-center justify-between p-4">
+      <div className="flex flex-col md:flex-row w-full max-w-[1096px] mx-auto items-center justify-between p-4 gap-4">
         <div className="flex items-center space-x-4">
           <Image src={'https://github.com/ecthon.png'} alt="Avatar" width={64} height={64} className="rounded-full" />
           <div className="flex flex-col">
@@ -39,23 +39,49 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="w-[1112px] mx-auto flex gap-4">
-        <div className="group cursor-pointer w-[360px] h-[420px] border border-zinc-100 rounded-2xl p-1">
-          <div className="flex flex-col justify-end h-full bg-gray-100 rounded-[12px] overflow-hidden">
-            <div className="flex w-full items-center p-4 bg-white/20 backdrop-blur-sm">
-              <h2 className="text-md font-semibold text-zinc-900">Nome do projeto.</h2>
+      <div className="w-full max-w-[1096px] mx-auto flex flex-col md:flex-row gap-2 max-md:px-4">
+        <div id="col-1" className="flex flex-col w-full md:w-[360px]">
+          <div className="group cursor-pointer w-full h-[300px] md:h-[420px] border border-zinc-100 rounded-2xl p-1">
+            <div className="flex flex-col justify-end h-full bg-gray-100 rounded-[12px] overflow-hidden">
+              <div className="flex h-32 w-full items-end p-4 bg-gradient-to-t from-black/10 to-transparent/0 backdrop-blur-sm">
+                <h2 className="text-md font-semibold text-white">Nome do projeto.</h2>
+              </div>
             </div>
           </div>
         </div>
-        <div className="w-[360px] bg-gray-100 p-4 rounded-2xl">
-          <h2 className="text-lg font-semibold mb-2">Card 2</h2>
-          <p>Conteúdo do card 2.</p>
+
+        <div id="col-2" className="flex flex-col w-full md:w-[360px] gap-2">
+          <div className="group cursor-pointer w-full h-[300px] md:h-[260px] border border-zinc-100 rounded-2xl p-1">
+            <div className="flex flex-col justify-end h-full bg-gray-100 rounded-[12px] overflow-hidden">
+              <div className="flex h-32 w-full items-end p-4 bg-gradient-to-t from-black/10 to-transparent/0 backdrop-blur-sm">
+                <h2 className="text-md font-semibold text-white">Nome do projeto.</h2>
+              </div>
+            </div>
+          </div>
+          <div className="group cursor-pointer w-full h-[300px] md:h-[260px] border border-zinc-100 rounded-2xl p-1">
+            <div className="flex flex-col justify-end h-full bg-gray-100 rounded-[12px] overflow-hidden">
+              <div className="flex h-32 w-full items-end p-4 bg-gradient-to-t from-black/10 to-transparent/0 backdrop-blur-sm">
+                <h2 className="text-md font-semibold text-white">Nome do projeto.</h2>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="w-[360px] bg-gray-100 p-4 rounded-2xl">
-          <h2 className="text-lg font-semibold mb-2">Card 3</h2>
-          <p>Conteúdo do card 3.</p>
+
+        <div id="col-3" className="flex flex-col w-full md:w-[360px]">
+          <div className="group cursor-pointer w-full h-[300px] md:h-[420px] border border-zinc-100 rounded-2xl p-1">
+            <div className="flex flex-col justify-end h-full bg-gray-100 rounded-[12px] overflow-hidden">
+              <div className="flex h-32 w-full items-end p-4 bg-gradient-to-t from-black/10 to-transparent/0 backdrop-blur-sm">
+                <h2 className="text-md font-semibold text-white">Nome do projeto.</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+      <footer className="w-full max-w-[1096px] mx-auto flex items-center justify-between p-4 text-zinc-500">
+        <p className="text-sm">© 2025 <b>Base²²</b></p>
+        <p className="text-sm">Made with ❤️ by <Link href={'#'} className="font-semibold hover:text-zinc-800">@ecthon</Link>.</p>
+      </footer>
     </div>
   );
 }
