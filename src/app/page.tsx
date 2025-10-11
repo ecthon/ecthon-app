7d2a62434e96e908b3c19bcf987df74ccd858379
ecthon <ecthon@gmail.com>
2025-10-11 00:07:40 -0400

    Refactor Profile component; update icon imports and enhance layout for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Home } from "@/components/home";
 
 export default function App() {
   return (
-    <div className="flex flex-col w-full space-y-6">
+    <div className="flex flex-col space-y-6">
       <Home />
       <div className="flex justify-center">
       </div>
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Clock01Icon, GithubIcon, Linkedin02Icon, Location01Icon, TelegramIcon } from "@hugeicons/core-free-icons";
+import { CheckmarkBadge01Icon, Clock01Icon, GithubIcon, Linkedin02Icon, Location01Icon, Message01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,17 +9,21 @@ import Clock from "./clock";
 export function Profile() {
     return (
         <div className="flex flex-col md:flex-row w-full max-w-[1096px] mx-auto items-center justify-between p-4 gap-4">
-            <div className="flex items-center space-x-4">
+            <div className="flex max-sm:flex-col max-sm:w-full max-sm:space-y-2 items-center space-x-4">
                 <Image src={'https://github.com/ecthon.png'} alt="Avatar" width={64} height={64} className="rounded-full border-2 border-zinc-300" />
-                <div className="flex flex-col">
-                    <p className="font-semibold text-zinc-900">Ecthon Almeida</p>
+                <div className="flex flex-col space-y-1">
+                    <div className="flex items-center gap-1">
+                        <p className="font-semibold text-zinc-900">Ecthon Almeida</p>
+                        <HugeiconsIcon icon={CheckmarkBadge01Icon} size={14} strokeWidth={2} className="text-lime-400 group-hover:text-lime-500" />
+                    </div>
+
                     <div className="flex items-center space-x-4">
                         <div className="group flex items-center space-x-1">
-                            <HugeiconsIcon icon={Location01Icon} size={14} strokeWidth={2} className="text-zinc-500 group-hover:text-orange-500" />
+                            <HugeiconsIcon icon={Location01Icon} size={14} strokeWidth={2} className="text-zinc-500 group-hover:text-lime-500" />
                             <p className="text-sm text-zinc-500">Manaus/AM</p>
                         </div>
                         <div className="group flex items-center space-x-1">
-                            <HugeiconsIcon icon={Clock01Icon} size={14} strokeWidth={2} className="text-zinc-500 group-hover:text-orange-500" />
+                            <HugeiconsIcon icon={Clock01Icon} size={14} strokeWidth={2} className="text-zinc-500 group-hover:text-lime-500" />
                             <Clock />
                         </div>
                     </div>
@@ -36,8 +40,8 @@ export function Profile() {
                     </Link>
                 </div>
                 <div className="flex h-5 w-0.5 rounded-full bg-zinc-300" />
-                <button className="group flex h-[32px] items-center gap-1 px-2.5 justify-center rounded-lg cursor-pointer font-semibold text-zinc-300 bg-zinc-900 text-xs">
-                    <HugeiconsIcon icon={TelegramIcon} size={18} strokeWidth={2} className="text-zinc-100 group-hover:text-orange-500 transition-colors duration-300" />
+                <button className="group flex h-[32px] items-center gap-1 px-2.5 justify-center rounded-lg cursor-pointer font-semibold text-zinc-100 bg-zinc-900 text-xs">
+                    <HugeiconsIcon icon={Message01Icon} size={18} strokeWidth={2} className="text-zinc-100 group-hover:text-lime-500 transition-colors duration-300" />
                     Send message
                 </button>
             </div>
