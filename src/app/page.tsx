309f497a5b469015f810e02e8174f5c09d851c1a
ecthon <ecthon@gmail.com>
2025-10-28 19:46:50 -0400

    Refactor layout in App and Home components for improved structure; add favicon.svg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { Home } from "@/components/home";
 
 export default function App() {
   return (
-    <div className="flex flex-col space-y-6">
+    <div className="flex w-full flex-col space-y-6">
       <Home />
-      <div className="flex justify-center">
-      </div>
     </div>
   );
 }
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -3,9 +3,9 @@ import { Projects } from "@/components/projects";
 
 export function Home() {
     return (
-        <>
+        <div className="flex flex-col w-full h-auto">
             <Profile />
             <Projects />
-        </>
+        </div>
     )
-} 
+}
