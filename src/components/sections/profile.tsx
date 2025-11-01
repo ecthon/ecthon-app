e391b54e54de6c9610e680ead207fd93b3dff080
ecthon <ecthon@gmail.com>
2025-11-01 12:31:15 -0400

    refactor: update Profile component to remove unused icons and enhance link structure

--- src/components/sections/profile.tsx
+++ src/components/sections/profile.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { CheckmarkBadge01Icon, Clock01Icon, GithubIcon, Linkedin02Icon, Location01Icon, Message01Icon } from "@hugeicons/core-free-icons";
+import { CheckmarkBadge01Icon, Clock01Icon, Location01Icon, Message01Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,7 +8,7 @@ import Clock from "../ui/clock";
 
 export function Profile() {
     return (
-        <div className="flex flex-col md:flex-row w-full max-w-[1096px] mx-auto items-center justify-between p-4 gap-4">
+        <div className="flex flex-col md:flex-row w-full h-screen max-w-[1096px] mx-auto items-center justify-normal p-4 gap-4">
             <div className="flex max-sm:flex-col max-sm:w-full max-sm:space-y-2 items-center space-x-4">
                 <Image src={'https://github.com/ecthon.png'} alt="Avatar" width={64} height={64} className="rounded-full border-2 border-zinc-300" />
                 <div className="flex max-sm:items-center flex-col space-y-1">
@@ -30,20 +30,19 @@ export function Profile() {
                 </div>
             </div>
 
-            <div className="flex items-center space-x-2">
-                <div className="flex items-center space-x-1">
-                    <Link href={'https://www.linkedin.com/in/ecthon/'} target="_blank" rel="noopener noreferrer" className="group flex p-1.5 border border-transparent max-sm:border-zinc-200 rounded-lg hover:bg-zinc-100 transition-colors">
-                        <HugeiconsIcon icon={Linkedin02Icon} size={18} strokeWidth={2} className="text-zinc-500" />
-                    </Link>
-                    <Link href={'https://github.com/ecthon'} target="_blank" rel="noopener noreferrer" className="group flex p-1.5 border border-transparent max-sm:border-zinc-200 rounded-lg hover:bg-zinc-100 transition-colors">
-                        <HugeiconsIcon icon={GithubIcon} size={18} strokeWidth={2} className="text-zinc-500" />
-                    </Link>
-                </div>
-                <div className="flex h-5 w-[1px] rounded-full bg-zinc-300" />
-                <button className="group flex h-[32px] items-center gap-1 px-2.5 justify-center rounded-lg cursor-pointer font-semibold text-indigo-600 hover:bg-indigo-600/25 bg-indigo-600/20 text-xs max-sm:text-[11px]">
-                    <HugeiconsIcon icon={Message01Icon} size={18} strokeWidth={2} className="text-indigo-600 transition-colors duration-300" />
-                    Send message
-                </button>
+            <div className="flex w-full flex-col space-y-2 my-5">
+                <Link href="" className="group w-[80%] mx-auto flex items-center space-x-2 bg-zinc-100 px-4 py-2 rounded-lg transition-colors duration-300 hover:bg-zinc-200">
+                    <HugeiconsIcon icon={Message01Icon} size={16} strokeWidth={2} className="text-zinc-500 group-hover:text-indigo-600" />
+                    <p className="text-zinc-500 group-hover:text-indigo-600 font-medium text-sm">site</p>
+                </Link>
+                <Link href="" className="group w-[80%] mx-auto flex items-center space-x-2 bg-zinc-100 px-4 py-2 rounded-lg transition-colors duration-300 hover:bg-zinc-200">
+                    <HugeiconsIcon icon={Message01Icon} size={16} strokeWidth={2} className="text-zinc-500 group-hover:text-indigo-600" />
+                    <p className="text-zinc-500 group-hover:text-indigo-600 font-medium text-sm">linkedin</p>
+                </Link>
+                <Link href="" className="group w-[80%] mx-auto flex items-center space-x-2 bg-zinc-100 px-4 py-2 rounded-lg transition-colors duration-300 hover:bg-zinc-200">
+                    <HugeiconsIcon icon={Message01Icon} size={16} strokeWidth={2} className="text-zinc-500 group-hover:text-indigo-600" />
+                    <p className="text-zinc-500 group-hover:text-indigo-600 font-medium text-sm">github</p>
+                </Link>
             </div>
         </div>
     )
