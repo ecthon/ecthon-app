5cb6e6fcad0c3115003101c70795b3d84dddfc71
ecthon <ecthon@gmailcom>
2026-03-15 00:51:55 -0400

    feat: Add initial home page with profile component and avatar image.

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex-col items-center justify-center w-3xl h-screen mx-auto">
-      <Profile />
       <div className="flex flex-col gap-4 my-20">
         <div className="flex flex-col gap-2">
           <p className="text-5xl w-2xs font-semibold text-white font-sans">Integrando</p>
-          <p className="text-5xl font-semibold text-white font-sans">design e código</p>
+          <p className="text-5xl font-semibold text-white font-sans">design e código.</p>
         </div>
-        <p className="text-lg text-white/60 font-sans">para criar interfaces que equilibram usabilidade, performance e estética.</p>
+        <p className="text-lg text-white/60 font-sans">Criando interfaces que equilibram usabilidade, performance e estética.</p>
       </div>
+      <Profile />
     </main>
   );
 }
--- components/profile.tsx
+++ components/profile.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export function Profile() {
     return (
         <div className="flex w-full items-center gap-4 p-4 border-b border-zinc-800">
-            <div className="flex items-center justify-between size-16 border-3 bg-white/20 border-white/20 rounded-full overflow-hidden">
-                <Image src="/ecthon.png" alt="Avatar" width={100} height={100} />
+            <div className="flex items-center justify-between size-12 border-dashed border-2 border-white/20 rounded-full overflow-hidden">
+                <Image src="/avatarbw.jpeg" alt="Avatar" width={100} height={100} />
             </div>
             <div className="flex flex-col">
                 <span className="text-sm font-semibold text-white font-sans">Ecthon Almeida</span>
