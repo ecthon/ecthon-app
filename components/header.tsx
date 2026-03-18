d73ddc9d3285d003fb9b63f46692b075dd91016f
ecthon <ecthon@gmail.com>
2026-03-18 00:11:14 -0300

    feat: Add Header component with a navigation link and fullscreen toggle functionality.

--- components/header.tsx
+++ components/header.tsx
@@ -29,7 +29,7 @@ export function Header() {
                 <h1 className="text-sm font-semibold text-white px-4 py-2 hover:bg-zinc-900 font-sans transition-colors duration-300 rounded-lg -ml-4 md:ml-0">ecthon.dev</h1>
             </Link>
             <div>
-                <button onClick={toggleFullscreen} className="group flex h-auto px-4 py-2 items-center justify-center hover:bg-zinc-900 transition-colors duration-300 rounded-lg gap-2 -mr-4 md:mr-0 cursor-pointer">
+                <button onClick={toggleFullscreen} className="group hidden sm:flex h-auto px-4 py-2 items-center justify-center hover:bg-zinc-900 transition-colors duration-300 rounded-lg gap-2 -mr-4 md:mr-0 cursor-pointer">
                     <p className="text-[11px] font-semibold text-zinc-200 font-sans transition-colors duration-300">{isFullscreen ? "Sair do modo tela cheia" : "Modo tela cheia"}</p>
                     <HugeiconsIcon icon={isFullscreen ? ArrowShrinkIcon : ArrowExpandIcon} size={16} color="currentColor" strokeWidth={2} className="text-zinc-200 transition-colors duration-300" />
                 </button>
