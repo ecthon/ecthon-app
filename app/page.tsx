071e101f361faafe17136ad97785246b10983da1
ecthon <ecthon@gmail.com>
2026-03-18 17:05:17 -0300

    feat: create initial home page with core sections

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Works } from "@/components/works";
 
 export default function Home() {
   return (
-    <main className="flex-col space-y-20 md:space-y-12 items-center justify-center w-full max-w-3xl px-6 md:px-0 min-h-screen mx-auto">
+    <main className="flex-col space-y-20 md:space-y-12 items-center justify-center w-full max-w-5xl px-6 md:px-0 min-h-screen mx-auto">
       <Header />
       <Hero />
       <Works />
