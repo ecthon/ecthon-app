e0008c0214f8afcf0f26757eafe8c3af2d750649
ecthon <ecthon@gmail.com>
2026-03-19 08:17:44 -0400

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -29,7 +29,7 @@ export function Hero() {
             <div className="flex flex-col gap-4 mt-8 mb-4 md:my-20">
                 <div className="flex flex-col gap-1 md:gap-2">
                     <p className="text-4xl md:text-5xl md:w-2xs font-semibold text-zinc-900 dark:text-white font-sans leading-tight">Integrando</p>
-                    <p className="text-4xl md:text-5xl font-semibold text-zinc-900 dark:text-white font-sans leading-tight">design e código.</p>
+                    <p className="text-4xl md:text-5xl font-semibold text-zinc-900 dark:text-white font-sans leading-tight">código e design.</p>
                 </div>
                 <p className="text-base md:text-lg text-zinc-500 dark:text-white/60 font-sans max-w-[280px] md:max-w-none">Criando interfaces que equilibram usabilidade, performance e estética.</p>
             </div>
