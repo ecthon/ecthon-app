74c4de1ccc46ad167c62ceb69bfb5e3708c52ee7
ecthon <ecthon@gmail.com>
2026-03-21 22:11:47 -0300

    feat: add Works component to display projects with a linked boarding pass app and construction placeholders.

--- components/works.tsx
+++ components/works.tsx
@@ -29,8 +29,8 @@ export function Works() {
                 <Link href="https://boarding-pass-seven.vercel.app/" target="_blank" rel="noopener noreferrer" className="flex w-full aspect-square md:aspect-auto md:row-span-2">
                     <div className="group relative flex border border-zinc-200 dark:border-zinc-900 items-center justify-center flex-1 h-full rounded-2xl overflow-hidden cursor-pointer bg-zinc-100 dark:bg-transparent">
                         <div className="flex w-full h-full flex-col justify-end transition-transform duration-500 ease-out group-hover:scale-110">
-                            <Image src="/img-ticket.webp" alt="Work 1" width={400} height={400} priority={true} quality={80} className="hidden md:block w-full h-full object-cover" />
-                            <Image src="/img-ticket-mobile.webp" alt="Work 1" width={400} height={400} priority={true} quality={80} className="md:hidden w-full h-full object-cover" />
+                            <Image src="/img-ticket.webp" alt="Work 1" width={1000} height={1000} priority={true} quality={80} className="hidden md:block w-full h-full object-cover" />
+                            <Image src="/img-ticket-mobile.webp" alt="Work 1" width={1000} height={1000} priority={true} quality={80} className="md:hidden w-full h-full object-cover" />
                         </div>
                         <div className="flex w-full h-full absolute inset-0 bg-linear-to-t from-black/80 dark:from-black/90 via-black/30 dark:via-black/40 to-transparent opacity-0 transition-opacity duration-500 ease-out group-hover:opacity-100 z-10 pointer-events-none" />
                         <div className="absolute bottom-0 left-0 w-full p-6 z-20 translate-y-4 opacity-0 transition-all duration-500 ease-out group-hover:translate-y-0 group-hover:opacity-100 flex pointer-events-none">
