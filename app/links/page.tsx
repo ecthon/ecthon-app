ee44a4e061c7f3f134cc0fb5b67f4c2a37dbabd9
ecthon <ecthon@gmail.com>
2026-03-21 22:01:18 -0300

    feat: add dedicated links page with profile and link cards.

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -4,13 +4,9 @@ import Image from "next/image";
 
 export default function LinksPage() {
     return (
-        <main className="flex flex-col w-full max-w-[600px] px-4 mx-auto justify-center space-y-12">
-            <div className="relative">
-                <Image src="/bg-links.jpeg" alt="Ecthon" width={1000} height={1000} quality={80} className="w-full h-full" />
-                <div className="absolute inset-0 bg-black/50"></div>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-8 -mt-50 z-10">
-                <Image src="/ecthon.jpeg" alt="Ecthon" width={1000} height={1000} quality={80} className="w-16 h-16 border-2 border-white rounded-full" />
+        <main className="flex flex-col w-full max-w-[600px] px-4 mx-auto justify-center py-24 space-y-12">
+            <div className="flex flex-col items-center justify-center gap-8">
+                <Image src="/ecthon.jpeg" alt="Ecthon" width={1000} height={1000} quality={80} className="w-16 h-16 rounded-full" />
                 <div className="flex flex-col items-center justify-center gap-2">
                     <p className="text-sm text-zinc-500 dark:text-zinc-400">@ecthon</p>
                     <div className="flex items-center gap-4">
@@ -19,7 +15,6 @@ export default function LinksPage() {
                     </div>
                 </div>
             </div>
-
             <div className="flex flex-col w-full items-center justify-center gap-2">
                 {myLinks.map((link) => (
                     <LinkCard
