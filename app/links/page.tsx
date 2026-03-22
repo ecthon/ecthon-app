d65c60868ba67805eb357bd713c4f8fd217e0989
ecthon <ecthon@gmailcom>
2026-03-21 23:00:37 -0400

    feat: add new links page with user profile and external links

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,30 +1,40 @@
 import { LinkCard } from "./link-card";
 import { myLinks } from "./all-links";
 import Image from "next/image";
+import { EnergyIcon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 
 export default function LinksPage() {
     return (
-        <main className="flex flex-col w-full max-w-[600px] px-4 mx-auto justify-center py-24 space-y-12">
-            <div className="flex flex-col items-center justify-center gap-8">
-                <Image src="/ecthon.jpeg" alt="Ecthon" width={1000} height={1000} quality={80} className="w-16 h-16 rounded-full" />
-                <div className="flex flex-col items-center justify-center gap-2">
-                    <p className="text-sm text-zinc-500 dark:text-zinc-400">@ecthon</p>
-                    <div className="flex items-center gap-4">
-                        <p className="text-2xl font-bold text-zinc-900 dark:text-zinc-100">×͜×</p>
-                        <h1 className="text-3xl font-bold text-zinc-900 dark:text-zinc-100">ecthon</h1>
+        <main className="flex flex-col w-full min-h-screen max-w-[600px] px-4 mx-auto py-8">
+            <div className="flex-1 flex flex-col justify-center w-full gap-12">
+                <div className="flex flex-col items-center justify-center gap-8">
+                    <Image src="/ecthon.jpeg" alt="Ecthon" width={1000} height={1000} quality={80} className="w-16 h-16 rounded-full" />
+                    <div className="flex flex-col items-center justify-center gap-2">
+                        <p className="text-sm text-zinc-500 dark:text-zinc-400">@ecthon</p>
+                        <div className="flex items-center gap-4">
+                            <p className="text-2xl font-bold text-zinc-900 dark:text-zinc-100">×͜×</p>
+                            <h1 className="text-3xl font-bold text-zinc-900 dark:text-zinc-100">ecthon</h1>
+                        </div>
                     </div>
                 </div>
+                <div className="flex flex-col w-full items-center justify-center p-2 gap-2">
+                    {myLinks.map((link) => (
+                        <LinkCard
+                            key={link.title}
+                            icon={link.icon}
+                            title={link.title}
+                            url={link.url}
+                        />
+                    ))}
+                </div>
             </div>
-            <div className="flex flex-col w-full items-center justify-center gap-2">
-                {myLinks.map((link) => (
-                    <LinkCard
-                        key={link.title}
-                        icon={link.icon}
-                        title={link.title}
-                        url={link.url}
-                    />
-                ))}
-            </div>
+            <footer className="flex w-full items-center justify-center mt-8 pb-4">
+                <div className="flex items-center gap-2 border border-zinc-200 dark:border-zinc-800 rounded-full pl-2 pr-4 py-1">
+                    <HugeiconsIcon icon={EnergyIcon} size={20} strokeWidth={2} className="text-zinc-600 dark:text-zinc-200" />
+                    <a href="https://ecthon.me" className="text-zinc-900 dark:text-zinc-100">ecthon</a>
+                </div>
+            </footer>
         </main>
     );
 }
